714405786c91bf96583e77726eaecdc3ebd44c00
SamuelMadeiraa <samuelmadeiradev@gmail.com>
2023-10-18 14:32:36 -0300

    Imprimindo 3 variaveis no console

---
+++ exercios-curso/ex001.js
@@ -0,0 +1,9 @@
+// imprimindo 3 variaveis diferentes
+
+var nome = "Samuel Madeira"
+var idade = 18
+var profissao = "Estagiario de programação"
+
+console.log(nome)
+console.log(idade)
+console.log(profissao)
