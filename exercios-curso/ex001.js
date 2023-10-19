f61f8f51d452fd362026ad68639a100c2e55cd5a
SamuelMadeiraa <samuelmadeiradev@gmail.com>
2023-10-19 14:26:39 -0300

    Concluindo o exercio 2

--- exercios-curso/ex001.js
+++
@@ -1,9 +0,0 @@
-// imprimindo 3 variaveis diferentes
-
-var nome = "Samuel Madeira"
-var idade = 18
-var profissao = "Estagiario de programação"
-
-console.log(nome)
-console.log(idade)
-console.log(profissao)
